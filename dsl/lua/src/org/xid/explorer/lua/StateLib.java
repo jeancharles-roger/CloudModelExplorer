edfe0c342d22c3a2abe3085246fcd3a9e5d97da7
Jean-Charles Roger <jeancharles.roger@gmail.com>
2014-12-12 09:28:36 +0100

    Adds getInt function

--- dsl/lua/src/org/xid/explorer/lua/StateLib.java
+++ dsl/lua/src/org/xid/explorer/lua/StateLib.java
@@ -3,6 +3,7 @@ package org.xid.explorer.lua;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.ThreeArgFunction;
+import org.luaj.vm2.lib.TwoArgFunction;
 import org.luaj.vm2.lib.jse.LuajavaLib;
 import org.xid.explorer.dsl.DslState;
 
@@ -17,13 +18,21 @@ public class StateLib extends LuajavaLib {
 	public LuaValue call(LuaValue env) {
 		final LuaTable state = new LuaTable();
 		setInt(state);
-		env.set("state", state);
+		getInt(state);
 
-		// Does I need to add it to loaded packages.
-		// Events is linked to one Program, it needs to be reloaded for another one
-		//env.get("package").get("loaded").set("state", state);
-		
+		env.set("state", state);
 		return state;
+	}
+
+	private void getInt(final LuaTable state) {
+		state.set("getInt", new TwoArgFunction() {
+			@Override
+			public LuaValue call(LuaValue luaState, LuaValue index) {
+				DslState state = (DslState) luaState.touserdata(DslState.class);
+				if (state == null) return LuaValue.NIL;
+				return LuaValue.valueOf(state.getInt(index.toint()));
+			}
+		});
 	}
 
 	private void setInt(final LuaTable state) {
@@ -31,7 +40,7 @@ public class StateLib extends LuajavaLib {
 			@Override
 			public LuaValue call(LuaValue luaState, LuaValue index, LuaValue value) {
 				DslState state = (DslState) luaState.touserdata(DslState.class);
-				if (state != null) return LuaValue.NIL;
+				if (state == null) return LuaValue.NIL;
 				state.setInt(index.toint(), value.toint());
 				return LuaValue.NIL;
 			}
